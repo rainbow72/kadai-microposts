b7bebafaf69d67918e5b4b5fdefb2ea657b8a573
golf.athlete <fortechachademy@gmail.com>
2021-06-21 11:06:34 +0000

    kadai bug fixed

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,14 +5,14 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.like(micropost)
     flash[:success] = "投稿のお気に入りを設定しました。"
-    redirect_to likes_user_path
+    redirect_to root_url
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unlike(micropost)
     flash[:success] = "投稿のお気に入りを解除しました。"
-    redirect_to likes_user_path
+    redirect_to root_url
   end
 
 end
